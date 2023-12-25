411bf56daed3e0885e25638e151a6d3a98467d92
Akash Holambe <akashholambe3@gmail.com>
2023-12-25 15:27:31 +0530

    save changes

--- Array/Array02.java
+++ Array/Array02.java
@@ -2,6 +2,23 @@ public class Array02 {
 
   public static void main(String[] args) {
     int arr[] = { 2, 4, 6, 8, 10, 12, 14, 16, 18 };
-    int key = 10;
+    int key = 18;
+    int start = 0;
+    int end = arr.length - 1;
+
+    while (start <= end) {
+        int mid = (start + end) / 2;
+  
+        if (arr[mid] == key) {
+          System.out.println("Key found at index: " + mid);
+          break; // exit the loop once the key is found
+        } else if (key > arr[mid]) {
+          start = mid + 1; // update the start index for the right half
+        } else {
+          end = mid - 1; // update the end index for the left half
+        }
+      }
   }
 }
+// we are finding index of 10 by using binary search
+// for finding elemet with binary seach we required array in sorted form.
--- Array/BinarySearch.java
+++ Array/BinarySearch.java
@@ -2,7 +2,7 @@ public class BinarySearch {
 
   public static void main(String[] args) {
     int arr[] = { 2, 3, 4, 5, 6, 7, 8, 9 };
-    int key = 8;
+    int key = 9;
     int start = 0;
     int end = arr.length - 1;
 
