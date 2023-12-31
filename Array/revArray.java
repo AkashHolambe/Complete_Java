cf506c268bc2669c562c961ff897e7f46cfcb9ca
Akash Holambe <akashholambe1999@gmail.com>
2023-12-31 17:48:02 +0530

    rev program

--- Array/revArray.java
+++ Array/revArray.java
@@ -1,12 +1,23 @@
 public class revArray {
-    public static void main(String[] args) {
 
-        int arr[] = { 4, 6, 7, 73, 8, 46, 8 };
-        int rev = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > rev) {
-                System.out.print(arr[i] + " ");
-            }
-        }
+  public static void main(String[] args) {
+    int arr[] = { 4, 6, 7, 3, 8, 6, 8 };
+    rev(arr);
+    for (int i = 0; i < arr.length; i++) {
+        System.out.print(arr[i]+" ");
     }
+  }
+
+  static void rev(int arr[]) {
+    int start = 0;
+    int end = arr.length - 1;
+    while (start < end) {
+      int temp = arr[end];
+      arr[end] = arr[start];
+      arr[start] = temp;
+
+      start++;
+      end--;
+    }
+  }
 }
