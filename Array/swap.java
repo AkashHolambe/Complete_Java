9ff3c6d6f8de692e8aa4b3bc3bd0fa1a1a37b528
Akash Holambe <akashholambe1999@gmail.com>
2023-12-31 17:26:01 +0530

    swaping of element

---
+++ Array/swap.java
@@ -0,0 +1,17 @@
+public class swap {
+   
+    
+    public static void main(String[] args) {
+        //here we are going to learn most easy and important concept in problem solving 
+
+        int a = 10;
+        int b = 15;
+
+
+        int temp = a;
+            a = b;
+            b = temp;
+            System.out.println("a : "+ a);
+            System.out.println("b : "+ b);
+    }
+}
