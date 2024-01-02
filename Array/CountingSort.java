1632fc7d0721067a227142599b3c1dc88356f24a
Akash Holambe <akashholambe1999@gmail.com>
2024-01-02 14:13:00 +0530

    Important changes

---
+++ Array/CountingSort.java
@@ -0,0 +1,13 @@
+public class CountingSort {
+
+  public static void main(String[] args) {
+    int arr[] = { 5, 4, 3, 1, 2 };
+    sort(arr);
+  }
+
+  public static void sort(int arr[]) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
+    }
+  }
+}
